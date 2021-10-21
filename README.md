93cf2d95d28755a0a5010ec0d258a314b572fdcb
777CyCLONE777 <jithujiji90@gmail.com>
2021-10-21 10:51:03 +0530

    First repo

--- README.md
+++ README.md
@@ -4,7 +4,11 @@
 - 💞️ I’m looking to collaborate on Discord maybe 💥
 - 📫 How to reach me getitback3207@gmail.com
 
-<!---
-777CyCLONE777/777CyCLONE777 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+
+- NJEN PAVEM..!
+
+
+
+
+
+           
---
+++ cyclonify.sh
@@ -0,0 +1,106 @@
+echo "wait a bit"
+sleep 3
+echo "Testing..."
+sleep 2
+
+apt install libcaca
+
+          clear
+
+
+          sleep 3
+
+
+echo "HELLO GUYS!" |lolcat
+sleep 2
+echo "LETS GET STARTED" |lolcat
+sleep 1
+echo "NOW WE'RE STARTING THE FILE EXECUTION " |lolcat
+sleep 2
+echo "BUT REMEMBER ONE THING THIS WILL AUTOMATICALLY TERMINATE" |lolcat
+sleep 4
+echo "AFTER THE EXECUTION" |lolcat
+sleep 2
+echo "SOOOOO...." |lolcat
+sleep 3 
+echo "SIT BACK AND ENJOY" |lolcat
+sleep 2
+             echo "ls"
+      sleep 2
+             ls
+      sleep 2
+           echo "WOW LOOK AT ALL THOSE FILE" |lolcat
+      sleep 3
+           echo "CAN I CREATE ONE FILE " |lolcat
+      sleep 3
+           echo "Please..." |lolcat
+           sleep 2
+           echo "Please Please Please" |lolcat
+                                  sleep 3
+                                 
+                 echo "I THINK YOU AGREED THAT " |lolcat
+                 sleep 2
+                 echo "CREATING A FILE...." |lolcat
+                 touch NjenPavem.sh
+          sleep 3
+              echo "All done" |lolcat
+           echo "Wait A Bit" |lolcat
+          sleep 4
+          echo "SUCCEFULLY CREATED A FILE" |lolcat
+        sleep 2
+        echo "Ls"
+        sleep 2
+        ls
+        echo "SOO THAT'S THAAT " |lolcat
+        sleep 3
+             echo "I'M ALLOWED TO CLEAR RIGHT?" |lolcat
+             sleep 4
+             echo "I Think..." |lolcat
+        sleep 2
+             echo "CLEARING THE SCREEN" |lolcat
+             sleep 3
+             clear
+             sleep 4 
+             echo "YAAH ITS THE TIME TO END" |lolcat
+        sleep 3
+        echo "Thanks For Executing" |lolcat
+              sleep 5
+             echo "waiting for ending :)" |lolcat
+
+         sleep 4
+             echo "MY CREATOR IS JITHU CYCLONE :)" |lolcat
+             sleep 3
+             echo "ooh one More Thing" |lolcat
+
+          
+
+            
+
+          sleep 3
+
+             echo "DON'T GO NOW WAIT"
+                          
+   sleep 4
+
+                                               echo "I'M TAKING MY FILE WITH ME " |lolcat
+
+
+sleep 5
+
+      rm -rf NjenPavem.sh
+
+               for i in {1..9}
+                    do
+                  echo "LOADING..." $i |lolcat
+                   sleep 1
+                        done
+
+
+                               echo "I GOT MY FILE " |lolcat
+                           sleep 4
+                                                          echo "soo goodbye" |lolcat
+
+  
+sleep 2
+                     echo "Ending..." |lolcat
+          cacafire
